fad513d7bdcc009212b6dcfff45acc6afb2612b1
ludooo <ludo.ac3@gmail.com>
2018-07-10 15:07:31 -0400

    Update buy-api.js

--- src/buy-api.js
+++ src/buy-api.js
@@ -53,6 +53,7 @@ const searchItems = function (searchConfig) {
     if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + encodeURI(JSON.stringify(searchConfig.filter).replace(/[{}]/g, "").replace(/"/g, ""));
     if (searchConfig.sort != undefined) queryParam = queryParam + "&sort=" + searchConfig.sort;
     if (searchConfig.category_ids != undefined) queryParam = queryParam + "&category_ids=" + searchConfig.category_ids;
+    console.log(queryParam);
     return new Promise((resolve, reject) => {
         makeRequest('api.ebay.com', `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
             resolve(result);
