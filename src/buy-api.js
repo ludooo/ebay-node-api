33d7a8a15c8d4b6df4711f6ed44e288606adab97
ludooo <ludo.ac3@gmail.com>
2018-07-10 15:06:55 -0400

    Update buy-api.js

--- src/buy-api.js
+++ src/buy-api.js
@@ -50,11 +50,9 @@ const searchItems = function (searchConfig) {
     queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
     queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
     if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups.toString();
-    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + JSON.stringify(searchConfig.filter).replace(/[{}]/g, "").replace(/"/g, "");
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + encodeURI(JSON.stringify(searchConfig.filter).replace(/[{}]/g, "").replace(/"/g, ""));
     if (searchConfig.sort != undefined) queryParam = queryParam + "&sort=" + searchConfig.sort;
     if (searchConfig.category_ids != undefined) queryParam = queryParam + "&category_ids=" + searchConfig.category_ids;
-    queryParam = encodeURI(queryParam);
-    console.log(queryParam);
     return new Promise((resolve, reject) => {
         makeRequest('api.ebay.com', `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
             resolve(result);
